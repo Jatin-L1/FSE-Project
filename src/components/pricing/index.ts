f9512f6a242f3dded67b389b04ba7731c40b1926
MOKSH88888 <practicemoksh@gmail.com>
2026-05-01 21:14:00 +0530

    add pricing page with plan cards, comparison table and FAQ

---
+++ src/components/pricing/index.ts
@@ -0,0 +1,5 @@
+export { default as PricingCard } from "./PricingCard";
+export type { Plan, PlanFeature } from "./PricingCard";
+
+export { default as PricingFAQ } from "./PricingFAQ";
+export { default as ComparisonTable } from "./ComparisonTable";
